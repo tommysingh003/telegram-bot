2e2bb73e13d37f2477dd099a86cad947abcc6acc
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-18 16:54:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 22926746  # api id
+API_HASH = "ffd91926f59e55fb08b3e1a4f5b99b1d"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "6223660988:AAFYthGatHBTspbuorp6aer2wv8KaBShZI4"  # bot token
 
 
 # REDIS
 HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "1"  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
+COOKIE = "y"
 ADMINS = [1317173146]
 
 
