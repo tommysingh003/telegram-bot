3219ed51cd730486e0fb672c70be5e25f671d9e0
tommysingh003 <tommysingh003@gmail.com>
2025-01-31 04:30:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -141,3 +141,8 @@ if __name__ == "__main__":
 
     logger.info("Bot is running...")
     application.run_polling()
+    def run_flask():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
+
+
